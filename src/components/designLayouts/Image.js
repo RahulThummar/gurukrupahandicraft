f837e27a8823664878547ccf29bcd8f8f7002f20
RahulThummar <rthummar788@rku.ac.in>
2023-12-23 08:59:22 +0530

    slider add

--- src/components/designLayouts/Image.js
+++ src/components/designLayouts/Image.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Image = ({ imgSrc, className }) => {
-  return <img className={className} src={imgSrc} alt={imgSrc} />;
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      {loading && (
+        <Skeleton width="100%" height="100%" style={{ position: "absolute" }} />
+      )}
+      <img
+        className={className}
+        src={imgSrc}
+        alt={imgSrc}
+        onLoad={handleImageLoad}
+        style={{ display: loading ? "none" : "block" }}
+      />
+    </div>
+  );
 };
 
 export default Image;
